cf404a34d01b0abd8ce96dab85ace55e6acbb754
Alif <m.alif.fadhillah32@gmail.com>
2024-03-26 16:04:18 +0800

    perf: adding table approve using sequelize

---
+++ models/tb_approve.js
@@ -0,0 +1,45 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class tb_approve extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  tb_approve.init(
+    {
+      id_tanaman: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      id_jenis: DataTypes.INTEGER,
+      id_kegiatan: DataTypes.INTEGER,
+      id_lokasi: DataTypes.INTEGER,
+      id_sk: DataTypes.INTEGER,
+      id_status: DataTypes.INTEGER,
+      diameter: DataTypes.DOUBLE,
+      tinggi: DataTypes.DOUBLE,
+      tanggal_tanam: DataTypes.DATE,
+      date_modified: DataTypes.DATE,
+      latitude: DataTypes.STRING,
+      longitude: DataTypes.STRING,
+      elevasi: DataTypes.STRING,
+      easting: DataTypes.STRING,
+      northing: DataTypes.STRING,
+      images: DataTypes.STRING,
+      verification: DataTypes.STRING,
+      uid: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "tb_approve",
+    }
+  );
+  return tb_approve;
+};
