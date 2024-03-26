42857bad5d25b1ed145ff9ea1b3e5191dd581f6f
Alif <m.alif.fadhillah32@gmail.com>
2024-03-26 15:28:27 +0800

    perf: adding table kegiatan using sequelize

---
+++ migrations/20240326072737-create-tb-kegiatan.js
@@ -0,0 +1,28 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("tb_kegiatans", {
+      id_kegiatan: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      kegiatan: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("tb_kegiatans");
+  },
+};
---
+++ models/tb_kegiatan.js
@@ -0,0 +1,29 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class tb_kegiatan extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  tb_kegiatan.init(
+    {
+      id_kegiatan: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      kegiatan: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "tb_kegiatan",
+    }
+  );
+  return tb_kegiatan;
+};
