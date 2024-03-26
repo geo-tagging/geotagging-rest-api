9ebda6b918cea30f4846931458497a275fadeb9c
Alif <m.alif.fadhillah32@gmail.com>
2024-03-26 15:30:43 +0800

    perf: adding table sk using sequelize

---
+++ migrations/20240326073009-create-tb-sk.js
@@ -0,0 +1,28 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("tb_sks", {
+      id_sk: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      skppkh: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("tb_sks");
+  },
+};
