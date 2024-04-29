b1097c16d9b1e462f5fe5bb58507a577c25b85f8
Alif <m.alif.fadhillah32@gmail.com>
2024-04-29 20:36:28 +0800

    fix

--- routes/verification.js
+++ routes/verification.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const verificationController = require("../controller/verification.controller");
-const imageUploader = require("../helper/image-uploader");
 const checkAuthMiddleware = require("../middleware/check-auth");
 const router = express.Router();
 
