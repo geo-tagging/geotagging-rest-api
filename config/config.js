664515543e297d42e606ff4bdceabcf71153f0e7
Alif <m.alif.fadhillah32@gmail.com>
2024-04-29 15:23:41 +0800

    perf: fixing config env

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
-const dotenv = require("dotenv").config().parsed;
+const dotenv = require("dotenv").config();
 
 let config = {
-  username: process.env["DB_USERNAME"],
-  password: process.env["DB_PASSWORD"],
-  database: process.env["DB_NAME"],
-  host: process.env["DB_HOST"],
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
   dialect: "mysql",
 };
 
