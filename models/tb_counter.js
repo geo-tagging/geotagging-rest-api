d6f4e633e74922cad4c6a248cf0dbb91b29d422e
Alif <m.alif.fadhillah32@gmail.com>
2024-04-29 16:10:06 +0800

    fix: fixing get counter endpoint

--- models/tb_counter.js
+++ models/tb_counter.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: "tb_counter",
-      tableName: "tb_counter",
+      tableName: "tb_counters",
     }
   );
   return tb_counter;
