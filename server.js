03c14fa1414151b635f98afa93f744e3f4302995
AlifFadhillahh <m.alif.fadhillah32@gmail.com>
2024-06-30 06:51:31 +0000

    fix : fixing api deployment code

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 const http = require("http");
 const app = require("./app");
 
-const port = process.env.PORT;
+// Default port to 8080 if process.env.PORT is not defined
+const port = process.env.PORT || 8080;
 
 const server = http.createServer(app);
 
