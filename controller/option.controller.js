95676a9eea5288ac2b3251cf2ff2a88962c36f55
Alif <m.alif.fadhillah32@gmail.com>
2024-09-05 23:52:03 +0800

    feat: add new endpoint for adding all kind of option

--- controller/option.controller.js
+++ controller/option.controller.js
@@ -39,6 +39,152 @@ function getAllOption(req, res) {
     });
 }
 
+function createNewOptionJenis(req, res) {
+  const jenis = {
+    nama: req.body.nama,
+    kategori: req.body.kategori,
+  };
+
+  const schema = {
+    nama: { type: "string", optional: false },
+    kategori: { type: "string", optional: false },
+  };
+
+  const v = new validator();
+  const validationResponse = v.validate(jenis, schema);
+
+  if (validationResponse !== true) {
+    return res.status(400).json({
+      message: "Validation failed",
+      error: validationResponse,
+    });
+  }
+
+  models.tb_jenis
+    .create(jenis)
+    .then((result) => {
+      res.status(201).json({
+        message: "New Plant Option Create Successfully",
+        post: result,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Something went wrong",
+        error: error,
+      });
+    });
+}
+
+function createNewOptionLokasi(req, res) {
+  const lokasi = {
+    lokasi: req.body.lokasi,
+  };
+
+  const schema = {
+    lokasi: { type: "string", optional: false },
+  };
+
+  const v = new validator();
+  const validationResponse = v.validate(lokasi, schema);
+
+  if (validationResponse !== true) {
+    return res.status(400).json({
+      message: "Validation failed",
+      error: validationResponse,
+    });
+  }
+
+  models.tb_lokasi
+    .create(lokasi)
+    .then((result) => {
+      res.status(201).json({
+        message: "New Location Option Create Successfully",
+        post: result,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Something went wrong",
+        error: error,
+      });
+    });
+}
+
+function createNewOptionSK(req, res) {
+  const skppkh = {
+    skppkh: req.body.skppkh,
+  };
+
+  const schema = {
+    skppkh: { type: "string", optional: false },
+  };
+
+  const v = new validator();
+  const validationResponse = v.validate(skppkh, schema);
+
+  if (validationResponse !== true) {
+    return res.status(400).json({
+      message: "Validation failed",
+      error: validationResponse,
+    });
+  }
+
+  models.tb_sk
+    .create(skppkh)
+    .then((result) => {
+      res.status(201).json({
+        message: "New skppkh Option Create Successfully",
+        post: result,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Something went wrong",
+        error: error,
+      });
+    });
+}
+
+function createNewOptionKegiatan(req, res) {
+  const kegiatan = {
+    kegiatan: req.body.kegiatan,
+  };
+
+  const schema = {
+    kegiatan: { type: "string", optional: false },
+  };
+
+  const v = new validator();
+  const validationResponse = v.validate(kegiatan, schema);
+
+  if (validationResponse !== true) {
+    return res.status(400).json({
+      message: "Validation failed",
+      error: validationResponse,
+    });
+  }
+
+  models.tb_kegiatan
+    .create(kegiatan)
+    .then((result) => {
+      res.status(201).json({
+        message: "New Activity Option Create Successfully",
+        post: result,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Something went wrong",
+        error: error,
+      });
+    });
+}
+
 module.exports = {
   getAllOption,
+  createNewOptionJenis,
+  createNewOptionLokasi,
+  createNewOptionSK,
+  createNewOptionKegiatan,
 };
--- routes/option.js
+++ routes/option.js
@@ -1,7 +1,32 @@
 const express = require("express");
 const optionController = require("../controller/option.controller");
+const checkAuthMiddleware = require("../middleware/check-auth");
 const router = express.Router();
 
-router.get("/", optionController.getAllOption);
+router.get("/", checkAuthMiddleware.checkAuth, optionController.getAllOption);
+
+router.post(
+  "/jenis",
+  checkAuthMiddleware.checkAuth,
+  optionController.createNewOptionJenis
+);
+
+router.post(
+  "/lokasi",
+  checkAuthMiddleware.checkAuth,
+  optionController.createNewOptionLokasi
+);
+
+router.post(
+  "/sk",
+  checkAuthMiddleware.checkAuth,
+  optionController.createNewOptionSK
+);
+
+router.post(
+  "/kegiatan",
+  checkAuthMiddleware.checkAuth,
+  optionController.createNewOptionKegiatan
+);
 
 module.exports = router;
